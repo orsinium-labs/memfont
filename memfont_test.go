18bd572aeb97e6c502dfe66ede4e11e8d8603eb5
gram <gram@orsinium.dev>
2020-11-09 14:47:11 +0100

    test size range, use assertify

--- memfont_test.go
+++ memfont_test.go
@@ -1,11 +1,12 @@
 package memfont_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/faiface/pixel/text"
-	"github.com/matryer/is"
 	"github.com/orsinium-labs/memfont"
+	"github.com/stretchr/testify/require"
 )
 
 type F = func() (*text.Atlas, error)
@@ -21,13 +22,36 @@ func funcs() map[string]F {
 }
 
 func TestNoErrors(t *testing.T) {
-	is := is.New(t)
 	for name, f := range funcs() {
 		t.Run(name, func(t *testing.T) {
-			is := is.New(t)
+			t.Parallel()
+			is := require.New(t)
 			atlas, err := f()
-			is.True(atlas != nil)
-			is.NoErr(err)
+			is.NotNil(atlas)
+			is.Nil(err)
 		})
+	}
+}
+
+func TestSizeRange(t *testing.T) {
+	for name, f := range funcs() {
+		for _, glyph := range "123456789qwertyQWERTYabcABC" {
+			name = fmt.Sprintf("%s/%c", name, glyph)
+			t.Run(name, func(t *testing.T) {
+				t.Parallel()
+				is := require.New(t)
+				atlas, err := f()
+				is.Nil(err)
+				is.True(atlas.Contains(glyph))
+				g := atlas.Glyph(glyph)
+				w := g.Frame.W()
+				h := g.Frame.H()
+
+				is.Greater(w, 2.0)
+				is.Greater(h, 2.0)
+				is.Less(w, 8.0)
+				is.Less(h, 7.0)
+			})
+		}
 	}
 }
